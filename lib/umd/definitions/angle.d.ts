426b57f039c2b69539d78ccf0a386f41f2699a2e
Boris Grigorov <borisgrigorov@vvveb.cz>
2025-11-15 20:47:17 +0100

    Compiled new slope unit

--- lib/umd/definitions/angle.d.ts
+++ lib/umd/definitions/angle.d.ts
@@ -1,7 +1,7 @@
 import { Measure } from './../index.js';
 export type AngleUnits = AngleSIUnits;
 export type AngleSystems = 'SI';
-export type AngleSIUnits = 'rad' | 'deg' | 'grad' | 'arcmin' | 'arcsec';
+export type AngleSIUnits = 'rad' | 'deg' | 'grad' | 'arcmin' | 'arcsec' | 'slope';
 declare const measure: Measure<AngleSystems, AngleUnits>;
 export default measure;
 //# sourceMappingURL=angle.d.ts.map
